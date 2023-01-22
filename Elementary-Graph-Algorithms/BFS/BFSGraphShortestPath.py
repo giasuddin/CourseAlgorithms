ba1458e76f4c602249e67aaec52e955080c1ed41
giasuddin <gias98@gmail.com>
2023-01-21 22:59:03 -0500

    bfs with sp

--- Elementary-Graph-Algorithms/BFS/BFSGraphShortestPath.py
+++ Elementary-Graph-Algorithms/BFS/BFSGraphShortestPath.py
@@ -1,53 +1,22 @@
-def bfs_sp(graph, start, goal=None):
-    explored = []
-    
-    # Queue for traversing the
-    # graph in the BFS
-    queue = [[start]]
-    
-    # If the desired node is
-    # reached
-    if start == goal:
-        print("Same Node")
-        return
-    
-    # Loop to traverse the graph
-    # with the help of the queue
+def bfs_sp(graph, start, end=None):
+    queue = [(start, 0, [start])]
+    visited = []
     while queue:
-        path = queue.pop(0)
-        node = path[-1]
-        
-        # Condition to check if the
-        # current node is not visited
-        if node not in explored:
-            neighbours = graph[node]
-            
-            # Loop to iterate over the
-            # neighbours of the node
-            for neighbour in neighbours:
-                new_path = list(path)
-                new_path.append(neighbour)
-                queue.append(new_path)
-                
-                # Condition to check if the
-                # neighbour node is the goal
-                if neighbour == goal:
-                    print("Shortest path = ", *new_path)
-                    return
-            explored.append(node)
-
-    # Condition when the nodes
-    # are not connected
-    if goal is None:
-        print("all explored =", explored)
+        node, distance, path = queue.pop(0)
+        if node not in visited:
+            visited.append(node)
+            if end is not None and node == end:
+                print("Shortest path found = ", path, ". Distance = ", distance)
+                return 
+            else:
+                for neighbor in graph[node]:
+                    new_path = path + [neighbor]
+                    queue.append((neighbor, distance + 1, new_path))
+    if end is None:
+        print("No end was given. So explored all vertices using BFS = ", visited)
     else:
-        print("So sorry, but a connecting"\
-                    "path doesn't exist :(")
-    
+        print("No path found!")
     return
-
-
-
 # Driver Code
 #if __name__ == "__main__":
 graph = {}
@@ -61,5 +30,6 @@ graph[6] = {4}
 bfs_sp(graph, 1, 6)
 bfs_sp(graph, 1)
 bfs_sp(graph, 5)
+bfs_sp(graph, 1, 7)
 
 
